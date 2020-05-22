73e6c99a1784ccae832aab5d3d7e8cfa810406b1
devinkr <git@dkr.im>
2020-05-22 18:57:12 -0500

    Added bullets

--- settings.py
+++ settings.py
@@ -4,10 +4,18 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings"""
         # Screen Settings
-        self.fullscreen = True
-        self.screen_width = 800
+        self.fullscreen = False
+        self.screen_width = 1024
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
         
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 1
+
+        # Bullet settings
+        self.bullet_speed = .75
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
