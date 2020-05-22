52015a5ef633ff839f5d92dda179173df899aa33
devinkr <git@dkr.im>
2020-05-22 18:03:41 -0500

    Added ship with left and right movment and full screen option

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        # Screen Settings
+        self.fullscreen = True
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+        
+        # Ship settings
+        self.ship_speed = 1.5
